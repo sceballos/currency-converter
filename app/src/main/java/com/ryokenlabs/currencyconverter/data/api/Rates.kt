f373b0d07d7c0c8e315a8250e2ed9dadb14fceea
sceballos <mobile.dev.learnings@gmail.com>
2021-09-02 20:32:27 +0900

    improvement(build): removed kotlin-android-extensions deprecated message by using proper implementation

--- app/src/main/java/com/ryokenlabs/currencyconverter/data/api/Rates.kt
+++ app/src/main/java/com/ryokenlabs/currencyconverter/data/api/Rates.kt
@@ -3,7 +3,7 @@ package com.ryokenlabs.currencyconverter.data.api
 import android.os.Parcelable
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Rates(
