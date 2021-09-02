f69bc6559b2376878f5b7d4cd5c3356b4242adea
sceballos <mobile.dev.learnings@gmail.com>
2021-09-02 20:06:20 +0900

    setup(data-class-utils): added kotlin android extensions to use @Parcelize annotation

---
+++ app/src/main/java/com/ryokenlabs/currencyconverter/data/api/Rates.kt
@@ -0,0 +1,33 @@
+package com.ryokenlabs.currencyconverter.data.api
+
+import android.os.Parcelable
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Rates(
+    @SerializedName("success")
+    @Expose
+    var success : String,
+
+    @Expose
+    @SerializedName("terms")
+    var terms : String,
+
+    @SerializedName("privacy")
+    @Expose
+    var privacy : String,
+
+    @SerializedName("timestamp")
+    @Expose
+    var timestamp : String,
+
+    @SerializedName("source")
+    @Expose
+    var source : String,
+
+    @SerializedName("quotes")
+    @Expose
+    var quotes : Map<String,String>
+    ) : Parcelable
